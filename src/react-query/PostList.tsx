417b19b6de571b4dd461cd9304fa2111eaf6e898
YOOUMUU <yooumuu@gmail.com>
2023-05-05 00:37:40 +0800

    Exercise: fetching data

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,6 +1,3 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import usePost from './hooks/usePost';
 
 const PostList = () => {
