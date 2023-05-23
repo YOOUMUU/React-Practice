232d25628cae188ec0fbdaf670f050a2f16c49eb
YOOUMUU <yooumuu@gmail.com>
2023-05-23 23:53:06 +0800

    Getting data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,21 @@
+import { useLocation, useParams, useSearchParams } from 'react-router-dom';
+
 const UserDetailPage = () => {
+  const params = useParams();
+  // console.log(params); // {id: '1'}
+  const [searchParams, setSearchParams] = useSearchParams();
+  // console.log(searchParams.toString()); // name=Alice&age=25
+  // console.log(searchParams.get('name')); // Alice
+  const location = useLocation();
+  // console.log(location);
+  //   {
+  //     "pathname": "/users/2",
+  //     "search": "?name=Alice&age=25",
+  //     "hash": "",
+  //     "state": null,
+  //     "key": "default"
+  // }
+
   return <p>User</p>;
 };
 
