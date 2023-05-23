d14b0fa0b8f391c518da735c36a4bf2d6f93a777
YOOUMUU <yooumuu@gmail.com>
2023-05-24 02:30:52 +0800

    Handling errors

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
 import HomePage from './HomePage';
 import Layout from './Layout';
 import UserDetailPage from './UserDetailPage';
-import UserList from './UserList';
 import UsersPage from './UsersPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
