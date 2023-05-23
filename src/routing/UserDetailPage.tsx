eb73c3230fa30595e909a5fd939f94eb56ee0cc2
YOOUMUU <yooumuu@gmail.com>
2023-05-24 01:53:01 +0800

    Exercise: Working with nested routes

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -2,21 +2,10 @@ import { useLocation, useParams, useSearchParams } from 'react-router-dom';
 
 const UserDetailPage = () => {
   const params = useParams();
-  // console.log(params); // {id: '1'}
   const [searchParams, setSearchParams] = useSearchParams();
-  // console.log(searchParams.toString()); // name=Alice&age=25
-  // console.log(searchParams.get('name')); // Alice
   const location = useLocation();
-  // console.log(location);
-  //   {
-  //     "pathname": "/users/2",
-  //     "search": "?name=Alice&age=25",
-  //     "hash": "",
-  //     "state": null,
-  //     "key": "default"
-  // }
 
-  return <p>User</p>;
+  return <p>User {params.id}</p>;
 };
 
 export default UserDetailPage;
