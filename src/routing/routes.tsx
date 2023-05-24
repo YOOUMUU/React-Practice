1b66c77141c052cfe1bcab91b0493ac45158eb48
YOOUMUU <yooumuu@gmail.com>
2023-05-24 18:18:39 +0800

    Layout routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -5,6 +5,7 @@ import Layout from './Layout';
 import UserDetailPage from './UserDetailPage';
 import UsersPage from './UsersPage';
 import LoginPage from './LoginPage';
+import PrivateRoutes from './PrivateRoutes';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,11 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: '/login', element: <LoginPage /> },
+    ],
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [
       {
         path: 'users',
         element: <UsersPage />,
